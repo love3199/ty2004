3b70565ea30ed8bfa37bfe9695c5af5eb973a6ce
love3199 <278852026@qq.com>
2020-10-15 14:26:21 +0800

    10.new2实现拖拽功能

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function drag(){
-
+function drag(selector){
+    let ele = document.querySelector(selector);
+    ele.onmousedown = function(evt){
+        let e = evt || window.event;
+        let disX = e.offsetX;
+        let disY = e.offsetY;
+        document.onmousemove = function(evt){
+            let e = evt || window.event;
+            ele.style.left = e.pageX - disX + 'px';
+            ele.style.top = e.pageY - disY + 'px';
+        }
+        document.onmouseup = function(){
+            document.onmousemove = null;
+        }
+        document.ondragstart = function(){
+            return false;
+        }
+    }
 }
